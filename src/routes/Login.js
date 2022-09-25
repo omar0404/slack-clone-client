1b84c0dd5e08e82b7a28ee358b181b45792dedda
omar-afifi <omar.afifi@vodafone.com>
2022-09-25 22:47:36 +0200

    navigate to home after succesful login  and set user session

--- src/routes/Login.js
+++ src/routes/Login.js
@@ -1,11 +1,18 @@
 import { gql, useMutation } from "@apollo/client"
 import { useRef } from "react";
+import { useNavigate } from "react-router-dom";
 import { Button, Input, Container, Header } from 'semantic-ui-react';
+import useSession from "../hooks/useSession";
 
 const loginMutation = gql`
   mutation($email:String!,$password:String!){
     login(email:$email,password:$password){
       ok
+      user {
+        id
+        username
+        email
+      }
       token
       refreshToken
       errors {
@@ -17,14 +24,15 @@ const loginMutation = gql`
 `;
 const Login = () => {
   const [login, { data, loading, error }] = useMutation(loginMutation)
+  const navigate = useNavigate()
+  const { setSession } = useSession()
   const formState = useRef({}).current
   const onSubmit = async () => {
-    console.log(...Object.values(formState))
-    const { data: { login: { token } } } = await login({ variables: formState })
-    localStorage.setItem('token', JSON.stringify(token))
+    const { data } = await login({ variables: formState })
+    localStorage.setItem('token', JSON.stringify(data?.login?.token))
+    setSession(data?.login?.user)
+    navigate('/')
   }
-  console.log('data', data)
-  console.log('error',)
   if (loading)
     return <h2>LOADING</h2>
   return (
